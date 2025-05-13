70778aca8b57cf0bd153a4b15fe6c2d439ca6fd3
YoumsCo <youmsc.co@gmail.com>
2025-05-14 00:38:38 +0100

    new file:   front-end/.eslintrc.json 	new file:   front-end/.gitignore 	new file:   front-end/README.md 	new file:   front-end/next.config.ts 	new file:   front-end/package-lock.json 	new file:   front-end/package.json 	new file:   front-end/postcss.config.js 	new file:   front-end/postcss.config.mjs 	new file:   front-end/public/favicon.ico 	new file:   front-end/src/pages/_app.tsx 	new file:   front-end/src/pages/_document.tsx 	new file:   front-end/src/pages/fonts/GeistMonoVF.woff 	new file:   front-end/src/pages/fonts/GeistVF.woff 	new file:   front-end/src/pages/index.tsx 	new file:   front-end/src/styles/globals.css 	new file:   front-end/src/ui/components/seo.tsx 	new file:   front-end/tailwind.config.js 	new file:   front-end/tailwind.config.ts 	new file:   front-end/tsconfig.json
     Changes not staged for commit:
    	modified:   front-end/.eslintrc.json
    	modified:   front-end/next.config.ts
    	modified:   front-end/package-lock.json
    	modified:   front-end/package.json
    	modified:   front-end/src/pages/_app.tsx
    	modified:   front-end/src/pages/index.tsx
    	modified:   front-end/src/styles/globals.css
    	deleted:    front-end/src/ui/components/seo.tsx
    	modified:   front-end/tailwind.config.js
    	modified:   front-end/tsconfig.json
    
     Untracked files:
    	CODERS MONKEYS.code-workspace
    	back-end/
    	front-end/.firebase/
    	front-end/.firebaserc
    	front-end/.hintrc
    	front-end/CODERS MONKEYS.code-workspace
    	front-end/firebase.json
    	front-end/public/asset/
    	front-end/src/api/
    	front-end/src/config/
    	front-end/src/context/
    	front-end/src/hooks/
    	front-end/src/lib/
    	front-end/src/pages/connexion/
    	front-end/src/pages/design-system.tsx
    	front-end/src/pages/mon-espace/
    	front-end/src/pages/onboarding.tsx
    	front-end/src/types/
    	front-end/src/ui/components/breadcrumbs/
    	front-end/src/ui/components/container/
    	front-end/src/ui/components/layout/
    	front-end/src/ui/components/navigation/
    	front-end/src/ui/components/seo/
    	front-end/src/ui/components/session/
    	front-end/src/ui/design-system/
    	front-end/src/ui/modules/

---
+++ front-end/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Seo } from "@/ui/components/seo";
+
+export default function Home() {
+  return <>
+
+    <Seo title="My first react with nextjs app" description="My first react with next js app">
+
+    </Seo>
+  </>
+  
+}
